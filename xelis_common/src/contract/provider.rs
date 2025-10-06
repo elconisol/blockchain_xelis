aa0275bde2d4e7a54c983796b117c9bc54fd2efe
elconisol <felixcolberts0316@gmail.com>
2025-10-06 23:32:56 +0900

    Update provider.rs

--- xelis_common/src/contract/provider.rs
+++ xelis_common/src/contract/provider.rs
@@ -4,32 +4,64 @@ use crate::{
     account::CiphertextCache,
     asset::AssetData,
     block::TopoHeight,
-    crypto::{Hash, PublicKey}
+    crypto::{Hash, PublicKey},
 };
 
 use super::ContractStorage;
 
+/// A trait providing high-level read access to contract-related data.
+/// Implementations must wrap or extend a lower-level [`ContractStorage`].
 pub trait ContractProvider: ContractStorage + 'static {
-    // Returns the balance of the contract
-    fn get_contract_balance_for_asset(&self, contract: &Hash, asset: &Hash, topoheight: TopoHeight) -> Result<Option<(TopoHeight, u64)>, anyhow::Error>;
+    /// Returns the balance of a contract for a specific asset.
+    fn get_contract_balance_for_asset(
+        &self,
+        contract: &Hash,
+        asset: &Hash,
+        topoheight: TopoHeight,
+    ) -> Result<Option<(TopoHeight, u64)>, anyhow::Error>;
 
-    // Get the account balance for asset
-    fn get_account_balance_for_asset(&self, key: &PublicKey, asset: &Hash, topoheight: TopoHeight) -> Result<Option<(TopoHeight, CiphertextCache)>, anyhow::Error>;
+    /// Returns the account balance for an asset at a given topological height.
+    fn get_account_balance_for_asset(
+        &self,
+        key: &PublicKey,
+        asset: &Hash,
+        topoheight: TopoHeight,
+    ) -> Result<Option<(TopoHeight, CiphertextCache)>, anyhow::Error>;
 
-    // Verify if an asset exists in the storage
-    fn asset_exists(&self, asset: &Hash, topoheight: TopoHeight) -> Result<bool, anyhow::Error>;
+    /// Checks whether an asset exists in storage at a given height.
+    fn asset_exists(
+        &self,
+        asset: &Hash,
+        topoheight: TopoHeight,
+    ) -> Result<bool, anyhow::Error>;
 
-    // Load the asset data from the storage
-    fn load_asset_data(&self, asset: &Hash, topoheight: TopoHeight) -> Result<Option<(TopoHeight, AssetData)>, anyhow::Error>;
+    /// Loads full asset data (metadata, rules, etc.) from storage.
+    fn load_asset_data(
+        &self,
+        asset: &Hash,
+        topoheight: TopoHeight,
+    ) -> Result<Option<(TopoHeight, AssetData)>, anyhow::Error>;
 
-    // Load the asset supply
-    fn load_asset_supply(&self, asset: &Hash, topoheight: TopoHeight) -> Result<Option<(TopoHeight, u64)>, anyhow::Error>;
+    /// Loads the total supply of a given asset.
+    fn load_asset_supply(
+        &self,
+        asset: &Hash,
+        topoheight: TopoHeight,
+    ) -> Result<Option<(TopoHeight, u64)>, anyhow::Error>;
 
-    // Verify if the address is well registered
-    fn account_exists(&self, key: &PublicKey, topoheight: TopoHeight) -> Result<bool, anyhow::Error>;
+    /// Verifies if an account (public key) exists in the ledger.
+    fn account_exists(
+        &self,
+        key: &PublicKey,
+        topoheight: TopoHeight,
+    ) -> Result<bool, anyhow::Error>;
 }
 
-// This is a wrapper around the storage to allow for the storage to be passed in the Context
+/// A wrapper allowing a mutable [`ContractProvider`] to be passed within a [`Context`].
 pub struct ContractProviderWrapper<'a, S: ContractProvider>(pub &'a mut S);
 
-tid! { impl<'a, S: 'static> TidAble<'a> for ContractProviderWrapper<'a, S> where S: ContractProvider }
+tid! {
+    impl<'a, S: 'static> TidAble<'a> for ContractProviderWrapper<'a, S>
+    where
+        S: ContractProvider
+}
